ddadc679e084b844e81beb735c066ab01d9b91d9
Nils Olsson <nilso@enosis.net>
2018-11-13 21:36:18 -0800

    Update main (to be a commandline testing program)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <getopt.h>
 
 #include "acm/acm.h" // TODO: Non-absolute path (included via CMake)
 
@@ -12,10 +13,39 @@ std::ostream& operator<<(std::ostream& os, std::vector<int> obj)
 
 int main(int argc, char **argv)
 {
-  ACM hilbert(1, 4);
+  int a, b;
+  ACM *acm;
 
-  std::cout << hilbert << '\n';
-  std::cout << hilbert.factorizations(144) << '\n';
+  for (char c; (c = getopt(argc, argv, "hHa:b:")) != -1;) {
+    switch (c) {
+      case 'h':
+        std::cout << "Help!\n";
+        return 0;
 
+      case 'H':
+        a = 1;
+        b = 4;
+        break;
+
+      case 'a':
+        a = std::stoi(optarg);
+        break;
+
+      case 'b':
+        b = std::stoi(optarg);
+        break;
+
+      default:
+        std::cout
+          << "Error!\n";
+        return 1;
+    }
+  }
+
+  assert(a && b);
+  acm = new ACM(a, b);
+
+  int n = std::stoi(argv[optind]);
+  std::cout << acm->factorizations(n) << '\n';
   return 0;
 }
